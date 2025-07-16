f53bfd3880282cb7b26138567b568a09f5dec61d
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2025-07-16 19:13:45 -0400

    fix deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
-import autoprefixer from 'autoprefixer';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 	},
-	preprocess: [
-		preprocess({
-			scss: {
-				includePaths: ['./src/styles', './node_modules'],
-				prependData: `
-					@import "./src/styles/_functions.scss";
-					@import "./src/styles/tokens/_index.scss";
-					@import "./src/styles/mixins/_index.scss";
-				`
-			},
-			postcss: {
-				plugins: [
-					autoprefixer(),
-				]
-			}
-		}),
-	]
+	preprocess: vitePreprocess()
 };
 
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+					@import "./src/styles/_functions.scss";
+					@import "./src/styles/tokens/_index.scss";
+					@import "./src/styles/mixins/_index.scss";
+				`
+			}
+		}
+	}
 };
 
 export default config;
